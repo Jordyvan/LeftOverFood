eac91eff742ea96fdcbb9b7af18302bda326a362
Son Of Zeus <jordy.van@student.umn.ac.id>
2020-05-07 03:06:22 +0700

    Login FireBase

--- app/src/main/java/id/ac/umn/leftoverfood/MainActivity.java
+++ app/src/main/java/id/ac/umn/leftoverfood/MainActivity.java
@@ -1,5 +1,6 @@
 package id.ac.umn.leftoverfood;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,22 +11,27 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-    EditText username , pass;
+    EditText usernameET , passET;
     Button lgn;
     boolean match = false;
-    String username1;
+    String username, pass;
 
     private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
 
         lgn = findViewById(R.id.lgnBtn);
@@ -34,20 +40,30 @@ public class MainActivity extends AppCompatActivity {
         lgn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                username = findViewById(R.id.userET);
-                pass = findViewById(R.id.passET);
-                if(username.getText().toString().equals("admin") && pass.getText().toString().equals("admin")){
-                    Toast.makeText(MainActivity.this, "Login Success" ,Toast.LENGTH_LONG).show();
-                    Intent intent = new Intent(MainActivity.this, Home.class);
-                    intent.putExtra("role",1);
-                    startActivity(intent);
-                }
-                else {
-                    Toast.makeText(MainActivity.this, "Username and Password Don't Match" ,Toast.LENGTH_LONG).show();
-                }
+                usernameET = findViewById(R.id.userET);
+                passET = findViewById(R.id.passET);
+
+                username = usernameET.getText().toString();
+                pass = passET.getText().toString();
+
+                mAuth.signInWithEmailAndPassword(username,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Login Success", Toast.LENGTH_LONG).show();
+                            Intent intent = new Intent(MainActivity.this, Home.class);
+                            startActivity(intent);
+                        }else {
+                            Toast.makeText(MainActivity.this, "Username and Password Don't Match" ,Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
             }
         });
     }
+
+
 
 
 }
